0162e5211c00b1a27489a4364dd99ab14e001e92
Loris Wintjens <loris.wintjens@gmail.com>
2019-06-16 18:27:05 +0200

    kk

---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+List<Color> bgcolors = [
+  Colors.indigo,
+  Colors.amber,
+  Colors.redAccent,
+  Colors.deepOrangeAccent,
+  Colors.purpleAccent
+];
+
+Color dispcolor = Colors.blue;
---
+++ lib/facts.dart
@@ -0,0 +1,20 @@
+List<String> facts = [
+  "11% of people are left handed",
+  "August has the highest percentage of births",
+  "unless food is mixed with saliva you can't taste it",
+  "the average person falls asleep in 7 minutes",
+  "a bear has 42 teeth",
+  "an ostrich's eye is bigger than its brain",
+  "lemons contain more sugar than strawberries",
+  "8% of people have an extra rib",
+  "Ralph Lauren's original name was Ralph Lifshitz",
+  "the Hawaiian alphabet has 13 letters",
+  "reindeer like bananas",
+  "the longest recorded flight of a chicken was 13 seconds",
+  "birds need gravity to swallow",
+  "the most commonly used letter in the alphabet is E",
+  "the 3 most common languages in the world are Mandarin Chinese, Spanish and English",
+  "the longest recorded flight of a chicken was 13 seconds"
+];
+
+String dispfact = "11% of people are left handed";
